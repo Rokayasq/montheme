d5cfd832ca0ef89aad0ceba6ee59db57747fb1e8
Rokaya Squalli <rokayasqualli@gmail.com>
2023-11-08 18:56:41 +0100

    création d'une template pour la modale de contact

---
+++ templates_part.php
@@ -0,0 +1,15 @@
+<!-- The Modal -->
+<div id="myModal" class="modal">
+    <!-- Modal content -->
+    <div class="modal-content">
+        <div class="modal-header">
+            <img src='<?php echo get_stylesheet_directory_uri() ."/assets/Contact-header.png" ?>' >
+        </div>
+
+        <?php
+        echo do_shortcode('[contact-form-7 id="7deb8fa" title="Formulaire de contact 1"]');
+        ?>
+    </div>
+ </div>
+
+
