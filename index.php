c380f7cf4cde5d304e4bb28bdf4b5261f9cd91f5
Rokaya Squalli <rokayasqualli@gmail.com>
2023-11-14 11:22:52 +0100

    Ajout du hero header

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 get_header(); 
 ?>
-
-<div id="content" class="site-content">
+<div id="hero-content" class="site-content">
     <main id="main" class="site-main">
         <?php
         // Boucle WordPress pour récupérer les articles du type de contenu "photo"
         $args = array(
             'post_type' => 'photo', // Nom du custom post type
-            'posts_per_page' => -1,  // Afficher tous les articles, vous pouvez ajuster cela selon vos besoins
+            'posts_per_page' => 1,  // Afficher tous les articles, vous pouvez ajuster cela selon vos besoins
+            'orderby' => 'rand', // image aléatoire
         );
 
         $photo_query = new WP_Query($args);
@@ -18,15 +18,16 @@ get_header();
                 $photo_query->the_post();
         ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class('block'); ?>>
-
-                    <div class="entry-content">
-                        <?php
-                        // Ajoutez le lien vers l'article complet
-                        $post_url = get_permalink();
-                        ?>
-                        <a href="<?php echo esc_url($post_url); ?>">
-                            <?php the_content(); ?>
-                        </a>
+                    <div class="image-container">
+                        <div class="image">
+                            <?php
+                            // Afficher le contenu de la publication
+                            the_content();
+                            ?>
+                            <div class="image-title">
+                                    <h2>PHOTOGRAPHE EVENT</h2>
+                            </div>
+                        </div>
                     </div>
                 </article>
         <?php
@@ -40,6 +41,10 @@ get_header();
         ?>
     </main>
 </div>
+
+<?php
+get_template_part( 'templates_parts/photo_block' );
+?>
 
 <?php
 get_footer(); 
