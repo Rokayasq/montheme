960d2787feb376a71f97423e207f961659b3b8be
Rokaya Squalli <rokayasqualli@gmail.com>
2023-11-24 17:34:57 +0100

    Apellation de lightbox.js et font-awesome

--- functions.php
+++ functions.php
@@ -295,3 +295,22 @@ function filter_images_date() {
 
 add_action('wp_ajax_filter_images_date', 'filter_images_date');
 add_action('wp_ajax_nopriv_filter_images_date', 'filter_images_date');
+
+
+// LIGHTBOX
+function ajouter_lightbox_js() {    
+    wp_enqueue_script('lightbox', get_stylesheet_directory_uri() . '/js/lightbox.js', array('jquery'), false, true);
+}
+
+add_action('wp_enqueue_scripts', 'ajouter_lightbox_js');
+
+// font awesome
+function enqueue_font_awesome() {
+    wp_enqueue_style(
+        'font-awesome',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css',
+        array(),
+        '6.2.1'
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
